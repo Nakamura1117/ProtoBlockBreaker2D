ba26889e4e518a167f13c349f62be2c638f6296b
Nakamura1117 <200495316+Nakamura1117@users.noreply.github.com>
2026-03-26 22:54:00 +0900

    下記の作業を実施しました。 ・コメントの整理 ・変数名をforceDirectionからforceVectorへ変更

--- Assets/Scripts/Stage/AttackBallController.cs
+++ Assets/Scripts/Stage/AttackBallController.cs
@@ -1,6 +1,5 @@
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
 public class AttackBallController : MonoBehaviour
 {
@@ -15,37 +14,42 @@ public class AttackBallController : MonoBehaviour
     public StageManager stage;
     public float speed = 7.0f;
     public float moveValLimit = 0.5f;   // バーの移動量計算で使用する
-    public float revideVecVal = 0.2f;   // ボールの縦移動の最低値
+    public float revideVecVal = 0.5f;   // ボールの縦移動の最低値
     public float accelLimit = 10f; // ボール加速の上限
 
-    private PlayerController playerBar;
-    private Rigidbody2D rbody;
-    private Vector2 forceDirection;
+    private PlayerController playerBar; // プレイヤーのバー情報
+    private Rigidbody2D rbody;  // 自身のRigidbody情報
+    private Vector2 forceVector; // ボールを動かす力の方向
 
+    // 反射判定をするオブジェクトのタグの元情報（配列）
     [SerializeField]
     private string[] defaultTargetTag = new string[3]{
         "Block",
         "PlayerBar",
         "Wall"
     };
-    private List<string> targetTag = null;
+    private List<string> targetTag = null;  // 反射判定をするオブジェクトのタグ情報（処理の際に増減させる）
 
-    private float blinkTime = 3.0f;
-    private bool isBlink = true;
-    private bool isReturnH = false;
-    private bool isReturnV = false;
-    private float accel = 0.0f;
+    private float blinkTime = 3.0f; // 生成後の点滅時間
+    private bool isBlink = true;    // 点滅管理フラグ
+    private bool isReturnH = false; // 横方向の反射フラグ
+    private bool isReturnV = false; // 縦方向の反射フラグ
+    private float accel = 0.0f; // 加速値
+
+    private float cntTime;  // 経過時間カウント用変数
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        // 初期値設定
         transform.position = new Vector3(0, -1, 0);
         isReturnH = false;
         isReturnV = false;
         rbody = GetComponent<Rigidbody2D>();
-        forceDirection = Vector2.down;
+        forceVector = Vector2.down;
         targetTag = new List<string>(defaultTargetTag);
         accel = 0.0f;
+        cntTime = 0;
         isBlink = true;
     }
 
@@ -85,91 +89,104 @@ public class AttackBallController : MonoBehaviour
             }
             return;
         }
+        // 経過時間をカウント
+        cntTime += Time.deltaTime;
+        if (cntTime >= 0.1f)
+        {
+            cntTime = 0;
+        }
     }
 
     void FixedUpdate()
     {
-        if (isBlink) return;
+        if (isBlink) return;    // 点滅しているときは処理を実施しない
 
+        // 横方向の反射を実施
         if (isReturnH)
         {
-            forceDirection.x *= -1;
+            forceVector.x *= -1;
             isReturnH = false;
         }
+        // 縦方向の反射を実施
         if (isReturnV)
         {
-            forceDirection.y *= -1;
+            forceVector.y *= -1;
             isReturnV = false;
         }
 
-        if (rbody.linearVelocity != forceDirection * speed)
-        {
-            // Debug.Log("forceDirection" + forceDirection);
-            rbody.linearVelocity = forceDirection * speed + new Vector2(accel, accel);
-        }
-
+        // 力の方向＋スピード＋加速値　※加速値はブロック以外で反射した時に加算
+        rbody.linearVelocity = forceVector * speed + new Vector2(accel, accel);
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
+        // 接触したオブジェクトのタグを変数に取得
         string tag = collision.gameObject.tag;
-        // Debug.Log("Collision " + collision.gameObject.tag + ">> " + targetTag.Contains(tag));
 
-        Debug.Log("forceDirect : " + forceDirection);
+        // Debug.Log("Collision " + collision.gameObject.tag + ">> " + targetTag.Contains(tag));
+        // Debug.Log("forceDirect : " + forceVector);
+
+        // 衝突したタグが反射対象のタグか確認する
         if (targetTag.Contains(tag))
         {
             accel += 0.1f;  // 何かに当たった時に加速する
-            if (accel > accelLimit) accel = accelLimit;
+            if (accel > accelLimit) accel = accelLimit; // 加速上限
 
+            // バーに当たった際に、バーの移動に応じて横方向に力を加える
             if (tag == "PlayerBar")
             {
                 float moveVal = playerBar.MoveVal;
 
+                // 移動値の上限・加減で設定
                 if (moveVal > moveValLimit) moveVal = moveValLimit;
                 if (moveVal < -(moveValLimit)) moveVal = -(moveValLimit);
+
 
                 // バーが動いていたら、ボールにも横方向に力を加える
                 if (Mathf.Abs(moveVal) > 0)
                 {
                     //Debug.Log(moveNum);
 
-                    // 縦の移動量が少ないとnomalizedで0になってしまうため、最低値を保証する
-                    if (Mathf.Abs(forceDirection.y) < revideVecVal)
-                    {
-                        // 元の関数を書き換えたくないので一時的に関数を作る
-                        float val = Mathf.Abs(revideVecVal);     // 移動方向が上の場合はプラスの補正値を設定
-                        if (forceDirection.y < 0) val *= -1;   // 移動方向が下の場合はマイナスの補正値を設定
-                        forceDirection.y = val;
-                    }
-
                     // バーの移動量に合わせて、ボールの方向を再定義する。
-                    forceDirection = new Vector2(forceDirection.x + (moveVal * 10), forceDirection.y).normalized;
+                    forceVector = new Vector2(forceVector.x + (moveVal * 10), forceVector.y).normalized;
+                }
+
+                // 縦の移動量が少ないとnomalizedで0になってしまうため、最低値を保証する
+                if (Mathf.Abs(forceVector.y) < revideVecVal)
+                {
+                    // 元の関数を書き換えたくないので一時的に関数を作る
+                    float valY = Mathf.Abs(revideVecVal);     // 移動方向が上の場合はプラスの補正値を設定
+                    float valX = 1 - valY;
+                    if (forceVector.y < 0) valY *= -1;  // 移動方向が下の場合はマイナスの補正値を設定
+                    if (forceVector.x < 0) valX *= -1;  // 移動方向が左の場合はマイナスの補正値を設定
+
+                    forceVector = new Vector2(valX, valY).normalized;   // 補正した値で移動値を標準化
                 }
             }
 
+            // ブロックに当たった場合
             if (tag == "Block")
             {
                 collision.gameObject.GetComponent<BlockController>().BreakBlock();
                 accel = 0.0f;   // ブロックに当たった時、加速をリセットする
             }
 
-            if (ballMode != BallModeName.Penetration)
-            {
-                float diffY = collision.GetContact(0).point.y - transform.position.y;
-                float diffX = collision.GetContact(0).point.x - transform.position.x;
+            // 当たった位置を取得する（絶対座標を自身から見た座標にする）
+            float diffY = collision.GetContact(0).point.y - transform.position.y;
+            float diffX = collision.GetContact(0).point.x - transform.position.x;
 
-                Debug.Log("x:" + diffX + "、y:" + diffY);
-                if (diffX > 0 && forceDirection.x > 0 || diffX < 0 && forceDirection.x < 0)
-                {
-                    isReturnH = true;
-                }
-                if (diffY > 0 && forceDirection.y > 0 || diffY < 0 && forceDirection.y < 0)
-                {
-                    isReturnV = true;
-                }
+            // 当たった方向と進行方向が同じ場合は反射フラグを立てる
+            if ((diffX > 0 && forceVector.x > 0) || (diffX < 0 && forceVector.x < 0))
+            {
+                isReturnH = true;
+            }
+            if ((diffY > 0 && forceVector.y > 0) || (diffY < 0 && forceVector.y < 0))
+            {
+                isReturnV = true;
             }
         }
 
+        // タグ「Dead」に触れたらボールが落ちた判定
         if (tag == "Dead")
         {
             stage.BallDrop();
@@ -177,11 +194,13 @@ public class AttackBallController : MonoBehaviour
         }
     }
 
+    // 生成時にステージマネージャーを設定する用のメソッド
     public void SetStageManager(StageManager sm)
     {
         stage = sm;
     }
 
+    // 生成時にバーを設定する用のメソッド
     public void SetPlayerBar(PlayerController pc)
     {
         playerBar = pc;
